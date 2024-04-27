466004ebc61f890a74ad39ed671192fd4acec7a1
CalcLoverHK <0v0katai@gmail.com>
2024-04-27 13:11:22 +0800

    refactor: update hidden RAM initialization settings

--- src-fx/CB_io.c
+++ src-fx/CB_io.c
@@ -187,13 +187,13 @@ int HiddenRAM_MatAryRestore(){	//  HiddenRAM -> MatAry ptr
 void HiddenRAM_ExtFontAryInit() {
 	if ( EnableExtFont ) {
         p_ext_asc              = (unsigned char *)HiddenRAM_Top;
-        p_ext_asc_mini         = (unsigned char *)(p_ext_asc           + 95*8);
-        p_ext_kana_gaiji       = (unsigned char *)(p_ext_asc_mini      + 95*8);
-        p_ext_kana_gaiji_mini  = (unsigned char *)(p_ext_kana_gaiji    + 99*8);
-        HiddenRAM_Top               = ((char *)p_ext_kana_gaiji             + 99*8);
+        p_ext_asc_mini         = (unsigned char *)(p_ext_asc        + 95*8);
+        p_ext_kana_gaiji       = (unsigned char *)(p_ext_asc_mini   + 95*8);
+        p_ext_kana_gaiji_mini  = (unsigned char *)(p_ext_kana_gaiji + 99*8);
+        HiddenRAM_Top          = ((char *)p_ext_kana_gaiji          + 99*8);
 	} else {
         p_ext_asc              = (unsigned char *)font_asc;
-        p_ext_asc_mini         = (unsigned char *)font_asc_mini;
+        p_ext_asc_mini         = (unsigned char *)font_asc_mini + 32*8;
         p_ext_kana_gaiji       = (unsigned char *)font_kana_gaiji;
         p_ext_kana_gaiji_mini  = (unsigned char *)font_kana_gaiji_mini;
 	}
