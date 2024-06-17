b095e38e50ee3bf99cdb2df10b007af80a18b7eb
CalcLoverHK <0v0katai@gmail.com>
2024-06-17 17:52:31 +0800

    lib: support min() and max() functions

--- include/cbasic/CB.h
+++ include/cbasic/CB.h
@@ -1,22 +1,3 @@
-/* *****************************************************************************
- * CB.h -- Main header file
- * Copyright (C) 2015-2024 Sentaro21 <sentaro21@pm.matrix.jp>
- *
- * This file is part of C.Basic.
- * C.Basic is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2.0 of the License,
- * or (at your option) any later version.
- *
- * C.Basic is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with C.Basic; if not, see <https://www.gnu.org/licenses/>.
- * ************************************************************************** */
-
 #include <ctype.h>
 #include <fxlib.h>
 #include <stdio.h>
@@ -25,14 +6,10 @@
 #include <math.h>
 #include <timer.h>
 
-typedef struct {
-    double real;
-    double imag;
+typedef struct  {
+	double real;
+	double imag;
 } complex;
-
-typedef char bool;
-#define false 0
-#define true 1
 
 #include "CB_complex.h"
 #include "CB_interpreter.h"
@@ -52,8 +29,12 @@ typedef char bool;
 #include "CB_Matrix.h"
 #include "CB_Str.h"
 #include "CB_Help.h"
+
 #include "CB_error.h"
 #include "fx_syscall.h"
 #include "fxCG_Registers.h"
 #include "MonochromeLib.h"
 #include "CB_MonochromeLib.h"
+
+#define min(X, Y)  ((X) < (Y) ? (X) : (Y))
+#define max(X, Y)  ((X) > (Y) ? (X) : (Y))
