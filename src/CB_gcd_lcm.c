4d5df16d8df10dcb887c4445797534d83f6d9b17
CalcLoverHK <0v0katai@gmail.com>
2026-06-24 00:49:12 +0800

    general/lib: fix `GCD(` and `LCM(` skipping the first argument
    This closes #41.

--- src/CB_gcd_lcm.c
+++ src/CB_gcd_lcm.c
@@ -1,16 +1,17 @@
 #include "CB.h"
 #include "syntax.h"
 
-#define GCD_LCM_PARSER_BODY(TYPE, IDENTITY, LIST_EVAL, SCALAR_EVAL, MATRIX_READ, OP, ERROR_RET) \
-    TYPE result = (IDENTITY); \
-    dspflag = 0; \
+#define GCD_LCM_PARSER_BODY(TYPE, LIST_EVAL, SCALAR_EVAL, MATRIX_READ, OP, ERROR_RET) \
+    int start = ExecPtr; \
+    TYPE result = SCALAR_EVAL(SRC); \
     if (CB_MatListAnsreg >= 28) CB_MatListAnsreg = 28; \
-    LIST_EVAL(SRC); \
     if (dspflag >= 3) { \
         if (dspflag != 4) { \
             CB_Error(ArgumentERR); \
             return (ERROR_RET); \
         } \
+        ExecPtr = start; \
+        LIST_EVAL(SRC); \
         int list_idx = CB_MatListAnsreg; \
         int size = MatAry[list_idx].SizeA; \
         int base = MatAry[list_idx].Base; \
@@ -18,46 +19,46 @@
             result = OP(result, MATRIX_READ(list_idx, ptr, base)); \
         dspflag = 2; \
     } else { \
-        do { \
-            syntax_skip(','); \
+        while (SRC[ExecPtr] == ',') { \
+            ExecPtr++; \
             result = OP(result, SCALAR_EVAL(SRC)); \
-        } while (SRC[ExecPtr] == ','); \
+        } \
     } \
     syntax_skip(')'); \
     return result;
 
-static complex parser_cplx(char *SRC, complex identity, complex (*op)(complex, complex)) {
-    GCD_LCM_PARSER_BODY(complex, identity, Cplx_ListEvalsubTop, Cplx_EvalsubTop, Cplx_ReadMatrix, op, Int2Cplx(0))
+static complex parser_cplx(char *SRC, complex (*op)(complex, complex)) {
+    GCD_LCM_PARSER_BODY(complex, Cplx_ListEvalsubTop, Cplx_EvalsubTop, Cplx_ReadMatrix, op, Int2Cplx(0))
 }
 
-static double parser_dbl(char *SRC, double identity, double (*op)(double, double)) {
-    GCD_LCM_PARSER_BODY(double, identity, ListEvalsubTop, EvalsubTop, ReadMatrix, op, 0.0)
+static double parser_dbl(char *SRC, double (*op)(double, double)) {
+    GCD_LCM_PARSER_BODY(double, ListEvalsubTop, EvalsubTop, ReadMatrix, op, 0.0)
 }
 
-static int parser_int(char *SRC, int identity, int (*op)(int, int)) {
-    GCD_LCM_PARSER_BODY(int, identity, ListEvalIntsubTop, EvalIntsubTop, ReadMatrixInt, op, 0)
+static int parser_int(char *SRC, int (*op)(int, int)) {
+    GCD_LCM_PARSER_BODY(int, ListEvalIntsubTop, EvalIntsubTop, ReadMatrixInt, op, 0)
 }
 
 complex CBC_lcm(char *SRC) {
-    return parser_cplx(SRC, Int2Cplx(1), Cplx_fLCM);
+    return parser_cplx(SRC, Cplx_fLCM);
 }
 
 double CBD_lcm(char *SRC) {
-    return parser_dbl(SRC, 1.0, fLCM);
+    return parser_dbl(SRC, fLCM);
 }
 
 int CBI_lcm(char *SRC) {
-    return parser_int(SRC, 1, fLCMint);
+    return parser_int(SRC, fLCMint);
 }
 
 complex CBC_gcd(char *SRC) {
-    return parser_cplx(SRC, Int2Cplx(0), Cplx_fGCD);
+    return parser_cplx(SRC, Cplx_fGCD);
 }
 
 double CBD_gcd(char *SRC) {
-    return parser_dbl(SRC, 0.0, CB_gcd_dbl);
+    return parser_dbl(SRC, CB_gcd_dbl);
 }
 
 int CBI_gcd(char *SRC) {
-    return parser_int(SRC, 0, CB_gcd_int);
+    return parser_int(SRC, CB_gcd_int);
 }
